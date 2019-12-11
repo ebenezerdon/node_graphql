ebcaf337f6e48a4dc5738a515e65ecc9a363ed9c
Ebenezer Don <ebenezerdonu@gmail.com>
2019-12-11 17:48:37 +0100

    add schema and graphiql to /graphql route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('../schema');
+
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
 
 module.exports = router;
